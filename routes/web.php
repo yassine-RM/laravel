972c18ce132d4aac91aa2a1d373ffac19c6187ba
REMMANI <remmani@v12software.com>
2020-08-05 11:37:58 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/', 'V12Controller@index')->name('home');
+Route::resource('blogs', 'BlogController');
